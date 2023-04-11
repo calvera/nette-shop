892b212e768099a8e7f3f66885c398ca8bab1fac
Karel Sommer <karel.sommer@inventi.cz>
2023-04-11 11:58:47 +0200

    WIP

--- app/Presenters/ProductListPresenter.php
+++ app/Presenters/ProductListPresenter.php
@@ -5,15 +5,29 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 
+use Nette\Utils\Paginator;
+
 final class ProductListPresenter extends BasePresenter
 {
     use Trait\AddToBasketTrait;
 
-    public function renderDefault(): void
+    private const ITEMS_PER_PAGE = 2;
+
+    public function renderDefault(int $page = 1): void
     {
+        $count = $this->database
+            ->table('product')->count();
+
+        $paginator = new Paginator;
+        $paginator->setItemCount($count);
+        $paginator->setItemsPerPage(self::ITEMS_PER_PAGE);
+        $paginator->setPage($page);
+
         $this->template->products = $this->database
             ->table('product')
             ->order('created_at DESC')
-            ->limit(5);
+            ->page($page, self::ITEMS_PER_PAGE);
+
+        $this->template->paginator = $paginator;
     }
 }
